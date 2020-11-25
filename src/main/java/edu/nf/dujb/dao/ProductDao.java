9471df2795a0494317e70894ca48e3bc87abac24
blue <1324335861@qq.com>
2020-11-25 16:03:19 +0800

    commit project

---
+++ src/main/java/edu/nf/dujb/dao/ProductDao.java
@@ -0,0 +1,12 @@
+package edu.nf.dujb.dao;
+
+import edu.nf.dujb.entity.Product;
+
+/**
+ * @author djb
+ */
+public class ProductDao {
+    public void save(Product product){
+
+    }
+}
---
+++ src/main/java/edu/nf/dujb/entity/Product.java
@@ -0,0 +1,11 @@
+package edu.nf.dujb.entity;
+
+import java.math.BigDecimal;
+
+/**
+ * @author djb
+ */
+public class Product {
+    private String name;
+    private BigDecimal price;
+}
